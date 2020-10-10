414c0d68535a10ab8bcd3b0ea5ec9ab6230d74ba
kabundege <christophekwizera1@gmail.com>
2020-10-10 15:28:05 +0200

    skills icons

--- src/components/Dash/skills.js
+++ src/components/Dash/skills.js
@@ -1,34 +1,62 @@
 import React from 'react';
-import '../../scss/components/skills.scss'
+import '../../scss/components/skills.scss';
+import eye from '../../assets/eye.svg'
 
 export default () => {
     return(
         <div className="container skills">
-            <div>
+            <div className="front">
                 <section>
                     <h3>FrontEnd</h3>
+                    <button><i className="far fa-eye"></i></button>
                 </section>
                 <section>
-                    <h3>Back</h3>
-                    <button>View</button>
+                    <button>
+                        <img src={eye} alt="close eye"/>
+                    </button>
+                    <ul>
+                        <li><img src="https://img.icons8.com/nolan/30/html-5.png" alt="frontend icons"/> HTML5</li>
+                        <li><img src="https://img.icons8.com/color/30/000000/css3.png" alt="frontend icons"/> CSS3</li>
+                        <li><img src="https://img.icons8.com/nolan/30/javascript.png" alt="frontend icons"/> JavaScript</li>
+                        <li><img src="https://img.icons8.com/color/30/000000/sass.png" alt="frontend icons"/> SASS</li>
+                        <li><img src="https://img.icons8.com/color/30/000000/react-native.png" alt="frontend icons"/>  React.Js</li>
+                    </ul>
                 </section>
             </div>
-            <div>
+            <div className="back">
                 <section>
                     <h3>BackEnd</h3>
+                    <button><i className="far fa-eye"></i></button>
                 </section>
                 <section>
-                    <h3>Back</h3>
-                    <button>View</button>
+                    <button>
+                        <img src={eye} alt="close eye"/>
+                    </button>
+                    <ul>
+                        
+                    <li><img src="https://img.icons8.com/nolan/30/github.png" alt="backend icons"/>GitHub</li>
+                        <li><img src="https://img.icons8.com/nolan/30/javascript.png" alt="backend icons"/> <img src="https://img.icons8.com/color/48/000000/nodejs.png" alt="nodejs"/></li>
+                        <li><img src="https://img.icons8.com/color/30/000000/firebase.png" alt="backend icons"/> Firebase</li>
+                        <li><img src="https://img.icons8.com/nolan/30/mysql.png" alt="backend icons"/> Sequelize </li>
+                        <li><img src="https://img.icons8.com/nolan/30/heroku.png" alt="backend icons"/> Heroku</li>
+                    </ul>
                 </section>
             </div>
-            <div className="card">
-                <section className="front">
+            <div className="mobile">
+                <section >
                     <h3>Mobile</h3>
+                    <button><i className="far fa-eye"></i></button>
                 </section>
-                <section className="back">
-                    <h3>Back</h3>
-                    <button>View</button>
+                <section >
+                    <button>
+                        <img src={eye} alt="close eye"/>
+                    </button>
+                    <ul>
+                        <li><img src="https://img.icons8.com/color/30/000000/flutter.png" alt="mobile icons"/> Flutter</li>
+                        <li><img src="https://img.icons8.com/color/30/000000/react-native.png" alt="mobile icons"/> React_Native</li>
+                        <li><img src="https://img.icons8.com/color/30/000000/firebase.png" alt="mobile icons"/> Firebase</li>
+                        <li><img src="https://img.icons8.com/color/30/000000/mongodb.png" alt="mobile icons"/> MongoDB</li>
+                    </ul>
                 </section>
             </div>
         </div>
