bd22dde5d4ecbf1cc24c12031c8ebd57700aa519
kabundege <christophekwizera1@gmail.com>
2022-01-24 16:25:54 +0200

    menu fix

--- src/components/header.js
+++ src/components/header.js
@@ -54,7 +54,7 @@ const Header = () => {
             {
                 showModal &&
                 <section >
-                    <ul >
+                    <ul className="menu">
                         <li onClick={()=>toggleMenu(true)} className="font-bold icon text-2xl text-gray-700">
                             <NavHashLink to="#home" >
                                 Home
